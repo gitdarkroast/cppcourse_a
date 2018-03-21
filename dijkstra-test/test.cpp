59a315aa038b17636a813ae3c5877b21794046db
Hanif Ladha <HLadha@absolute.com>
2018-03-21 08:48:03 -0700

    Add project files.

---
+++ dijkstra-test/test.cpp
@@ -0,0 +1,116 @@
+#include "pch.h" 
+#include "..\dijkstra\dijkstra.h"
+
+namespace {
+
+	// Fixture class for the Vertex ADT
+	class VertexTest : public ::testing::Test
+	{
+
+	};
+
+	// Test cased for the Vertex ADT implementation
+	TEST(VertexTest, VertexObjExists)
+	{
+		ASSERT_TRUE(new Vertex(1));
+	}
+	
+	// Test the default Vertex Id
+	TEST(VertexTest, DefaultVertexId)
+	{
+		Vertex v{ 1 };
+		EXPECT_EQ(1, v.getID());
+	}
+
+	// Test the setting of the vertex id value
+	TEST(VertexTest, SetVertexIDValue)
+	{
+		int id = 10;
+		Vertex v{ id };
+		EXPECT_EQ(id, v.getID());
+	}
+	// Test default edge list
+	TEST(VertexTest, NeighborsEmpty)
+	{
+		int id = 10;
+		Vertex v{ id };
+		std::vector<Edge> list = v.neighbors();
+		EXPECT_EQ(0, list.size());
+	}
+
+	// Test default degree of vertex
+	TEST(VertexTest, VertexDegreeZero)
+	{
+		int id = 10;
+		Vertex v{ id };
+		EXPECT_EQ(0, v.degree());
+	}
+
+	// Test the default Edge constructor
+	TEST(EdgeTest, DefaultEdgeObject)
+	{
+		int id = 10;
+		Vertex v1{ id };
+		id = 11;
+		Vertex v2{ id };
+		Edge e{ v1, v2 };
+
+		EXPECT_EQ(-1, e.getCost());
+	}
+	// Test set edge weight
+	TEST(EdgeTest, SetEdgeWeight)
+	{
+		int id = 10;
+		Vertex v1{ id };
+		id = 11;
+		Vertex v2{ id };
+		Edge e{ v1, v2 };
+
+		e.setCost(11);
+		EXPECT_EQ(11, e.getCost());
+	}
+
+	// Fixture class for the Edge ADT
+	class EdgeTest : public ::testing::Test
+	{
+
+	};
+
+
+
+	// Fixture class for the Graph ADT
+	class GraphTest : public ::testing::Test
+	{
+	protected:
+	};
+	// Test cased for the Graph ADT implementation
+	TEST(GraphTest, GraphObjExists)
+	{
+		ASSERT_TRUE(new Graph());
+	}
+
+	// Test initial verticies
+	TEST(GraphTest, GraphEmptyVertices)
+	{
+		Graph g;
+		EXPECT_EQ(0, g.vertices());
+	}
+
+	// Test addition of 2 vertices
+	TEST(GraphTest, GraphAddTwoVertices)
+	{
+		Graph g;
+		Vertex v1{ 1 };
+		Vertex v2{ 2 };
+		//g.addVertex(v1);
+		EXPECT_EQ(1, g.vertices());
+		//g.addVertex(v2);
+		EXPECT_EQ(2, g.vertices());
+	}
+} // namespace
+
+int main(int argc, char **argv)
+{
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
---
+++ dijkstra/dijkstra.h
@@ -0,0 +1,152 @@
+#pragma once
+#include <iostream>
+#include <chrono>
+#include <vector>
+
+
+// Though not part of the assignment, the following class implements a Timer,
+// allowing you to check the runtime execution of the implementation.
+// Credit: http://www.learncpp.com/cpp-tutorial/8-16-timing-your-code/
+
+class Timer
+{
+private:
+	using clock_t = std::chrono::high_resolution_clock;
+	using second_t = std::chrono::duration<double, std::ratio<1>>;
+
+	std::chrono::time_point<clock_t> m_beg;
+
+public:
+	Timer() : m_beg(clock_t::now())
+	{}
+
+	void reset()
+	{
+		m_beg = clock_t::now();
+	}
+
+	double elapsed() const
+	{
+		return std::chrono::duration_cast<second_t>(clock_t::now() - m_beg).count();
+	}
+};
+
+
+// Forward declaration of Edge ADT
+class Edge;
+
+// Vertex ADT definition
+// The Vertex ADT consists of:
+//		- a vertex ID
+//		- a vector of edges - the destination end point being another vertex.
+class Vertex
+{
+private:
+	int m_id;											// ID to define the vertex
+	std::vector<Edge> m_edges;								// List of edges for this vertex
+public:
+	Vertex(int id) :m_id(id) {};						// Constructor, default ID is 0.
+	int getID() { return m_id; };	
+	void setID(int id) { m_id = id; };
+	int degree() { return m_edges.size(); };				// The number of neighbors this vertex has
+	std::vector<Edge> neighbors() { return m_edges; };		// The list edges from this vertex.
+	void addEdge(Edge e);
+	bool deleteEdge(Edge e);
+
+	friend bool operator==(const Vertex &v1, const Vertex &v2)
+	{
+		return v1.m_id == v2.m_id;
+	};
+};
+
+// Edge ADT definition
+// The Edge ADT consists of:
+//		- The endpoint pair (src,dst) of vertices
+//		- The cost of going from src to dst
+class Edge
+{
+private:
+	Vertex m_src;
+	Vertex m_dst;
+	int m_cost{ -1 };
+public:
+	Edge(Vertex v_src, Vertex v_dst) :m_src(v_src), m_dst(v_dst) {}
+	Vertex getSrc() { return m_src; };
+	Vertex getDst() { return m_dst; };
+	void setCost(int c) { m_cost = c; };
+	int getCost() { return m_cost; };
+
+	friend bool operator== (const Edge &e1, const Edge &e2)
+	{
+		return ((e1.m_src == e2.m_src) &&
+			(e1.m_dst == e2.m_dst));
+	}
+};
+
+// Edge List ADT definition
+/*
+Example:
+1->2->3->4
+2->4
+3->1->4
+4
+*/
+class EdgeList
+{
+private:
+	std::vector< std::vector<Edge> > m_representation;	 // A vector of Edges
+public:
+	EdgeList() {};
+	~EdgeList() {};
+
+	friend std::ostream& operator<<(std::ostream& out, const EdgeList& list);
+
+};
+
+// Connectivity Matrix ADT definition
+/*
+Example:
+	1	2	3	4
+1	0	1	1	1
+2	0	0	0	1
+3	0	0	0	1
+4	0	0	0	0
+*/
+class ConnectivityMatrix
+{
+private:
+	std::vector< std::vector<int> > m_representation;	// A two-dimensional array
+public:
+	ConnectivityMatrix() {};
+	~ConnectivityMatrix() {};
+
+	friend std::ostream& operator<<(std::ostream& out, const ConnectivityMatrix& matrix);
+};
+
+// Graph ADT definition
+/*
+The Graph ADT manages a set of nodes with a specific representation
+*/
+class Graph
+{
+private:
+	std::vector<Vertex> m_vertices;						// The set of vertices that make up this graph
+
+public:
+	Graph() {};
+	Graph(int size);
+	int vertices() { return m_vertices.size(); };		// The K value of the graph
+	bool adjacent(Vertex src, Vertex dst);
+	std::vector<Vertex> neighbors(Vertex v);
+	int getVertexID(Vertex v) { return v.getID(); };
+	void addVertex(Vertex v);
+	bool deleteVertex(Vertex v);
+};
+
+// openSet
+// a list of nodes with minimum cost
+
+
+// closedSet
+// a list of neighbors with associated cost
+
